f18b8f7ef92f1d4ced7702b8685d3bdb2bd69525
MichaelTrizel <michael.sa@ku.th>
2023-03-30 22:42:48 +0700

    NSC Grading Program V1.4

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory, session
 from jinja2 import Environment
-import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'upload_video'
