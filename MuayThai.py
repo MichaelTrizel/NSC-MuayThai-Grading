88a31193dea824fc0436f0d996b117023d6e9b5e
Sun Nattapat <nattapat.sud@ku.th>
2023-03-31 03:14:54 +0700

    NSC Grading Program V1.5

---
+++ MuayThai.py
@@ -0,0 +1,224 @@
+import pandas as pd
+import numpy as np
+import cv2
+import mediapipe as mp
+
+class MuayThai:
+    def __init__(self, path_clip_name, data, step, true_steps, cal_steps):
+        self.path_clip_name = path_clip_name
+        self.data = data
+        self.step = step
+        self.true_steps = true_steps
+        self.cal_steps = cal_steps
+
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_holistic = mp.solutions.holistic
+        self.mp_pose = mp.solutions.pose
+
+
+    def get_data_size(self):
+        return len(self.data)
+
+    def cal_angle_xy(self, threshold, body1, body2, body3):
+        a = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_x'.format(body1)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_y'.format(body1)]])
+        b = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_x'.format(body2)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_y'.format(body2)]])
+        c = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_x'.format(body3)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_y'.format(body3)]])
+
+        radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+        angle = int(np.abs(radians * 180.0 / np.pi))
+        if angle > 180:
+            angle = 360 - angle
+
+        return angle
+
+    def cal_angle_yz(self, threshold, body1, body2, body3):
+        a = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_z'.format(body1)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_y'.format(body1)]])
+        b = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_z'.format(body2)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_y'.format(body2)]])
+        c = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_z'.format(body3)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_y'.format(body3)]])
+
+        radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+        angle = int(np.abs(radians * 180.0 / np.pi))
+        if angle > 180:
+            angle = 360 - angle
+
+        return angle
+
+    def cal_angle_xz(self, threshold, body1, body2, body3):
+        a = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_x'.format(body1)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_z'.format(body1)]])
+        b = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_x'.format(body2)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_z'.format(body2)]])
+        c = np.array([self.data.loc[self.data['threshold'] == threshold, '{}_x'.format(body3)],
+                      self.data.loc[self.data['threshold'] == threshold, '{}_z'.format(body3)]])
+
+        radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+        angle = int(np.abs(radians * 180.0 / np.pi))
+        if angle > 180:
+            angle = 360 - angle
+
+        return angle
+
+    def find_angles_sub_step(self, step_data, threshold):
+        angles = []
+        for sub_step in range(1, len(step_data)+1):
+            filt = (step_data['sub_step'] == sub_step)
+            axis = step_data.loc[filt, 'axis'].iloc[0]
+
+            if axis == 'xy':
+                angles.append(self.cal_angle_xy(threshold, step_data.loc[filt, 'begin_node'].iloc[0],
+                                step_data.loc[filt, 'main_node'].iloc[0],
+                                step_data.loc[filt, 'end_node'].iloc[0]))
+            elif axis == 'yz':
+                angles.append(self.cal_angle_yz(threshold, step_data.loc[filt, 'begin_node'].iloc[0],
+                                step_data.loc[filt, 'main_node'].iloc[0],
+                                step_data.loc[filt, 'end_node'].iloc[0]))
+            elif axis == 'xz':
+                angles.append(self.cal_angle_xz(threshold, step_data.loc[filt, 'begin_node'].iloc[0],
+                                step_data.loc[filt, 'main_node'].iloc[0],
+                                step_data.loc[filt, 'end_node'].iloc[0]))
+        return angles
+
+    #return boolean
+    def check_sub_step(self, ans_step_data, angles):
+        checked_sub_step = 0
+        for sub_step in range(1, len(ans_step_data)+1):
+            filt = (ans_step_data['sub_step'] == sub_step)
+            compared = eval('{} {} {}'.format(angles[sub_step-1], ans_step_data.loc[filt, 'operator'].iloc[0],
+                                        ans_step_data.loc[filt, 'true_angle'].iloc[0]))
+            #print(ans_step_data)
+            if compared:
+                checked_sub_step += 1
+        return checked_sub_step == len(ans_step_data)
+
+    def check(self):
+        point = 0
+        curr_step = 1
+        thresholds = self.data['threshold'].tolist()
+        frame_correct = []
+        angles_correct = []
+        step_correct = []
+
+        for i in range(len(thresholds)):
+            threshold = thresholds[i]
+            all_angles = []
+
+            #Find angles for every step
+            for step in range(curr_step, self.step+1):
+                curr_step_data = self.cal_steps[self.cal_steps['step'] == step]
+                angle = self.find_angles_sub_step(curr_step_data, threshold)
+                #print(angle)
+                all_angles.append(angle)
+            step_round = 0
+            #Compare angles for every step
+            for step in range(curr_step, self.step+1):
+                curr_ans_step_data = self.true_steps[self.true_steps['step'] == step]
+                if self.check_sub_step(curr_ans_step_data, all_angles[step_round]):
+                    frame_correct.append(threshold)
+                    angles_correct.append(all_angles[step_round])
+                    point += 1
+                    
+                    step_correct.append(curr_ans_step_data['step'].iloc[0])
+                    curr_step = step+1
+                    
+                    #print('Frame: {}, True angle: {}'.format(int(threshold), all_angles[step_round]))
+                    #print('Current point: {}'.format(point))
+                    break
+                step_round += 1
+                
+            #print(all_angles)
+
+            if point == self.step:
+                break
+
+        return point, frame_correct, angles_correct, step_correct
+
+    def get_landmarks(self):
+        cap = cv2.VideoCapture(self.path_clip_name)
+        frameTime = 1
+        threshold = 0
+        results_list = []
+        diff = 4
+
+        with self.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+            while cap.isOpened():
+                threshold += 1
+
+                showed, frame = cap.read()
+                if showed:
+                    frame = cv2.resize(frame, (1920, 1080))
+
+                    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    img.flags.writeable = False
+
+                    results = holistic.process(img)
+
+                    try:
+                        landmarks = results.pose_landmarks.landmark
+                    except:
+                        pass
+
+                    img.flags.writeable = True
+                    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+                    self.mp_drawing.draw_landmarks(img, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+
+                    if threshold % diff == 0:
+                        #cv2.imshow('Mediapipe Feed', img)
+                        results_list.append(results)
+
+                    if cv2.waitKey(frameTime) & 0xFF == ord('q'):
+                        break
+                else:
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+            
+        return results_list
+
+    def show_true_graph(self, landmarks, true_answer_index):
+        for i in range(len(true_answer_index)):
+            self.mp_drawing.plot_landmarks(landmarks[true_answer_index[i]].pose_landmarks, self.mp_pose.POSE_CONNECTIONS, elevation=0, azimuth=0)
+    
+    '''
+    def find_similarity_with_angle(self, MuayThai(tester)):
+        # Find score & frame of self.df (trainer)
+        trainer_point, trainer_true_frame = self.check()
+        # Find score & frame
+        tester_point, tester_true_frame = MuayThai(tester).check()
+        
+        # Score of trainer and tester must max
+        if (trainer_point == self.step && tester_point == self.step):
+            #Get data frame of trainer and tester (get angle of bodies)
+            
+            #Find similarity between 2 frame using fastdtw
+            
+        elif (tester_point < self.step):
+            #Notice tester is not pass
+            
+            
+    '''
+    
+    '''
+    def find_similarity_with_axis(self, MuayThai(tester)):
+        # Find score & frame of self.df (trainer)
+        trainer_point, trainer_true_frame = self.check()
+        # Find score & frame
+        tester_point, tester_true_frame = MuayThai(tester).check()
+        
+        # Score of trainer and tester must max
+        if (trainer_point == self.step && tester_point == self.step):
+            #Get data frame of trainer and tester (get axis of bodies)
+            
+            #Find similarity between 2 frame using fastdtw
+            
+        elif (tester_point < self.step):
+            #Notice tester is not pass
+            
+    '''
--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import os
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory, session
 from jinja2 import Environment
 
+import mediapipe as mp
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
+
+from Converter import Conveter
+from MuayThai import MuayThai
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'upload_video'
 app.config['TUTORIAL_FOLDER'] = 'tutorial'
@@ -57,6 +66,9 @@ def pier_selection():
 
 		session['video_file'] = video_file.filename
 		session['pier'] = pier
+		session['video_path'] = video_path
+
+		print(video_path)
 
 		return render_template(selected_pier + '.html', video_file=video_file.filename, pier=pier)
 	else:
@@ -74,10 +86,169 @@ def tutorial(filename):
 # def image(filename):
 # 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+def find_missing(lst, step):
+	step_missed = []
+	for i in range(1, step+1):
+		if lst.count(i) == 0:
+			step_missed.append(i)
+	return step_missed
+
 @app.route('/loading')
 def loading():
 	video_file = session.get('video_file',None)
 	pier = session.get('pier',None)
+	video_path = session.get('video_path',None)
+
+	#Model code
+	user_folder = './user_data/'
+	if not os.path.exists(user_folder):
+		os.makedirs(user_folder)
+
+	user_converter = Conveter(video_path, user_folder)
+	clip_df = user_converter.convert_video_to_node()
+	user_converter.write_video_node_to_csv(clip_df, 'user.csv')
+
+
+	if pier == "ท่าสลับฟันปลา":
+		true_df = pd.read_csv('./trainer/1/true.csv')
+		cal_df = pd.read_csv('./trainer/1/cal.csv')
+		trainer_df = pd.read_csv('./trainer/1/clip.csv')
+
+		#Dtw
+		number_pier = 1
+		distance_point = 50
+
+		#Grading
+		grade_df = pd.read_csv('./trainer/1/grade.csv')
+		mean = 41.3333303030303
+		std = 32.76412259432642
+
+	elif pier == "ท่าตาเถรค้ำพัก":
+		true_df = pd.read_csv('./trainer/6/true.csv')
+		cal_df =pd.read_csv('./trainer/6/cal.csv')
+		trainer_df = pd.read_csv('./trainer/6/clip.csv')
+
+		#Dtw
+		number_pier = 6
+		distance_point = 160
+
+		#Grading
+		grade_df = pd.read_csv('./trainer/6/grade.csv')
+		mean = 41.3333303030303
+		std = 32.76412259432642
+
+	elif pier == "ท่ามอญยันหลัก":
+		true_df = pd.read_csv('./trainer/7/true.csv')
+		cal_df = pd.read_csv('./trainer/7/cal.csv')
+		trainer_df = pd.read_csv('./trainer/7/clip.csv')
+
+		#Dtw
+		number_pier = 7
+		distance_point = 50
+
+		#Grading
+		grade_df = pd.read_csv('./trainer/7/grade.csv')
+		mean = 41.3333303030303
+		std = 32.76412259432642
+
+	elif pier == "ท่าดับชวาลา":
+		true_df = pd.read_csv('./trainer/13/true.csv')
+		cal_df = pd.read_csv('./trainer/13/cal.csv')
+		trainer_df = pd.read_csv('./trainer/13/clip.csv')
+
+		#Dtw
+		number_pier = 13
+		distance_point = 200
+
+		#Grading
+		grade_df = pd.read_csv('./trainer/13/grade.csv')
+		mean = 41.3333303030303
+		std = 32.76412259432642
+
+	elif pier == "ท่าหักคอเอราวัณ":
+		true_df = pd.read_csv('./trainer/15/true.csv')
+		cal_df = pd.read_csv('./trainer/15/cal.csv')
+		trainer_df = pd.read_csv('./trainer/15/clip.csv')
+
+		#Dtw
+		number_pier = 15
+		distance_point = 150
+
+		#Grading
+		grade_df = pd.read_csv('./trainer/15/grade.csv')
+		mean = 41.3333303030303
+		std = 32.76412259432642
+
+	del true_df['Unnamed: 0']
+	del cal_df['Unnamed: 0']
+
+	user_df = pd.read_csv('./user_data/user.csv')
+	del user_df['Unnamed: 0']
+	user_muay = MuayThai(video_path, user_df, 4, true_df, cal_df)
+	user_point, user_true_frames, user_true_angles, user_true_steps = user_muay.check()
+	#print(user_point)
+
+	#Seperate to each result page
+	# This is result_2 
+	if (user_point < user_muay.step):
+		print('bad')
+		step_missed = find_missing(user_true_steps, 4)
+		
+		#Answer
+		step_missed_str = ', '.join(map(str, step_missed))
+		print(step_missed_str)
+	
+	# This is result_1
+	else:
+		print('good')
+
+		max_angles_len_user = len(max(user_true_angles, key=len))
+		user_padding_angle = [np.pad(arr, 
+                             pad_width=max_angles_len_user-len(arr), 
+                             mode='constant', 
+                             constant_values=0)[max_angles_len_user-len(arr):] for arr in user_true_angles]
+
+		clip_max = 35
+		point_criterion = 0
+
+		for i in range(1, clip_max+1):
+			trainer_muay = MuayThai(''.format(i), 
+							trainer_df[trainer_df['clip_name'] == '{}_{}'.format(number_pier, i)], 
+							4, true_df, cal_df)
+			
+			trainer_point, trainer_true_frames, trainer_true_angles, trainer_true_steps = trainer_muay.check()
+			max_angles_len_train = len(max(trainer_true_angles, key=len))
+			trainer_padding_angle = [np.pad(arr, 
+									pad_width=max_angles_len_train-len(arr), 
+									mode='constant', 
+									constant_values=0)[max_angles_len_train-len(arr):] for arr in trainer_true_angles]
+			
+			distance, path = fastdtw(trainer_padding_angle, user_padding_angle, dist=euclidean)
+			if (distance <= distance_point):
+				point_criterion += 1
+
+		#Answer of similarity
+		similarity = (point_criterion/clip_max)*100
+		print(similarity)
+
+		#Answer of grade
+		z_score = (similarity-mean)/std
+		t_score = (z_score*10)+50
+
+		print('T_score', t_score)
+
+		grade = 'N'
+		if t_score >= grade_df.loc[grade_df['grade'] == 'A', 'min'].iloc[0]:
+			grade = 'A'
+		elif t_score >= grade_df.loc[grade_df['grade'] == 'B', 'min'].iloc[0] and t_score < grade_df.loc[grade_df['grade'] == 'B', 'max'].iloc[0]:
+			grade = 'B'
+		elif t_score >= grade_df.loc[grade_df['grade'] == 'C', 'min'].iloc[0] and t_score < grade_df.loc[grade_df['grade'] == 'C', 'max'].iloc[0]:
+			grade = 'C'
+		elif t_score < grade_df.loc[grade_df['grade'] == 'D', 'max'].iloc[0]:
+			grade = 'D'
+		print(grade)
+	#End of model code
+
 	if video_file is None or pier is None:
 		return redirect('/')
 	return render_template('loading.html', video_file=video_file, pier=pier)
